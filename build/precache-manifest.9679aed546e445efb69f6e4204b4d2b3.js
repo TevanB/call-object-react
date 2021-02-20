a3a005fa236cd0454942ffc9e481c6dbe440b06b
TevanB <tevan.b@gmail.com>
2021-02-19 23:28:27 -0500

    init

---
+++ build/precache-manifest.9679aed546e445efb69f6e4204b4d2b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27e63c8c7388a6f2f54aa9dc33e3033b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe8a661be5362ef1c4b1",
+    "url": "/static/css/main.d7b62ddd.chunk.css"
+  },
+  {
+    "revision": "f7dd289279cb575b398c",
+    "url": "/static/js/2.38706c38.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.38706c38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe8a661be5362ef1c4b1",
+    "url": "/static/js/main.097e0e12.chunk.js"
+  },
+  {
+    "revision": "712a493cb6bbaa95b925",
+    "url": "/static/js/runtime-main.cef06927.js"
+  }
+]);
